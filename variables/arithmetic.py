ea17052648a15211a5feb1ff41f5efe8d99c43fa
Md. Tahmid Chowdhury <mdtahmidchowdhury12@gmail.com>
2022-07-13 19:44:31 +0600

    Variables work

---
+++ variables/arithmetic.py
@@ -0,0 +1,30 @@
+a = 35
+b = 98
+
+# Arithmetic operator
+print("a + b = ", a + b)
+print("a - b = ", a - b)
+print("a * b = ", a * b)
+
+# Assignment operator
+c = 5
+d = 6
+
+print(c,d)
+c += 5
+d += 1
+print(c, d)
+
+# Comparison Operator
+e = 5
+f = 8
+
+print(e==f)
+print(e>f)
+print(e<f)
+print(e!=f)
+
+# Logical Operator
+print(e==f and e>f)
+print(e==f and e<f)
+print(not(e>f))
---
+++ variables/input.py
@@ -0,0 +1,6 @@
+a = input("Enter 1st value: ")
+b = input("Enter 2nd value: ")
+
+print(type(a), type(b))
+
+print("The total Sum is:", int(a) + int(b))
