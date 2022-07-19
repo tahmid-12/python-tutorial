cb9f117efa36d5121a4e2f32f42dcd1b39c533fa
Md. Tahmid Chowdhury <mdtahmidchowdhury12@gmail.com>
2022-07-19 23:47:32 +0600

    Dictionary in progress

---
+++ Dictionary/01_dict.py
@@ -0,0 +1,8 @@
+apps = {
+    "facebook" : "informstion App",
+    "instagram": "Picture uploading App",
+    "youtube": "Video sharing app",
+    "people": ["a","b",5,8]
+}
+
+print(apps["instagram"])
