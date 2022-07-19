599c4279add426791d0986b9a19babc683e02f5b
Md. Tahmid Chowdhury <mdtahmidchowdhury12@gmail.com>
2022-07-19 23:56:38 +0600

    In progress Dictionary

--- Dictionary/01_dict.py
+++ Dictionary/01_dict.py
@@ -5,4 +5,20 @@ apps = {
     "people": ["a","b",5,8]
 }
 
-print(apps["instagram"])
+# Accessing a list index in a dictionary
+# print(apps["people"][3])
+
+# Accessing a key in the dictionary
+# print(apps["youtube"])
+
+# Returns a tuple of key, value pair
+# print(apps.items())
+
+# For loop in dictionary
+# for a,b in apps.items():
+#     print(a, "=>", b)
+
+# Prints all the keys in the dictionary
+for key in apps.keys():
+    print(key)
+
